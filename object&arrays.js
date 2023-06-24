6c46c44d4c22c6f81155dbdc67203663206c91ee
Sohail Shah <132382358+sohail052@users.noreply.github.com>
2023-06-24 20:55:43 +0500

    Create object&arrays.js
    > Basic of Object & Arrays.
    > How to store values in objects and arrays.
    > How to print values from objects and arrays.

---
+++ object&arrays.js
@@ -0,0 +1,20 @@
+//STORE & PRINT VALUES IN OBJECT...//
+
+
+let obj1 = {         //Always use comma (,) @ object values end.
+  name: "Shah",
+  Rollno: 45,
+  Seatno: 17250000,
+  Dept: "Computer Science",
+  Class: "Javascript"
+}
+
+console.log(obj1)      //Print all values of object "obj1"
+
+
+//STORE & PRINT VALUES IN ARRAY...//
+
+let arr1 = ["Sohail", 10]
+
+console.log(arr1[0]);    //Print values @ index[0] = 1st value stored in array "arr1"
+console.log(arr1[1]);    //Print values @ index[1] = 2nd value stored in array "arr1"
